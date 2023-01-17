ebcc7a0de16c7a3e5dc7b214a6a15d3811315a07
cyl19970726 <15258378443@163.com>
2023-01-17 22:02:12 +0800

    refactor: update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,10 +3,20 @@ import { ethers } from "hardhat";
 async function main() {
   const Git3 = await ethers.getContractFactory("Git3Hub");
   const git3 = await Git3.deploy();
-  let receipt = await git3.deployed();
+  let logicReceipt = await git3.deployed()
 
-  console.log(receipt);
-  console.log(git3.address);
+  let factory1 = await ethers.getContractFactory("UpgradeableProxy");
+  // Proxy don't need to init Git3 contract because the constructor is empty.
+  let initSelector = "0x";
+  let [operator,] = await ethers.getSigners();
+  let proxyInstance = await factory1
+    .connect(operator)
+    .deploy(git3.address, operator.address, initSelector);
+  let proxyReceipt = await proxyInstance.deployed()
+
+  // console.log({logicReceipt,proxyReceipt});
+  console.log("Logic Contract",git3.address);
+  console.log("Proxy Contract",git3.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
